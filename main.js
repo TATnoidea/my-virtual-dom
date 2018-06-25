47ac941cf38bf6ca7ea6dc1e379051f688674b3d
chengzichao <chengzichao@lansheng8.com>
2018-06-25 10:12:15 +0800

    添加babel

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
+'use strict';
+
 /** @jsx h */
 
-function h(type, props, ...children) {
-  return { type, props, children };
+function h(type, props) {
+  for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    children[_key - 2] = arguments[_key];
+  }
+
+  return { type: type, props: props, children: children };
 }
 
 function createElement(node) {
   if (typeof node === 'string') {
     return document.createTextNode(node);
   }
-  const $el = document.createElement(node.type);
-  node.children
-  .map(createElement)
-  .forEach($el.appendChild.bind($el));
+  var $el = document.createElement(node.type);
+  node.children.map(createElement).forEach($el.appendChild.bind($el));
   return $el;
 }
 
-const a = (
-  <ul class="list">
-    <li>item 1</li>
-    <li>item 2</li>
-  </ul>
+var a = h(
+  'ul',
+  { 'class': 'list' },
+  h(
+    'li',
+    null,
+    'item 1'
+  ),
+  h(
+    'li',
+    null,
+    'item 2'
+  )
 );
 
-const $root = document.getElementById('root');
+var $root = document.getElementById('root');
 $root.appendChild(createElement(a));
